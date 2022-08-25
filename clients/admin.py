6f0b5c5169dfe07d894c8720eb6406575bb504a8
almazbarat <almazbekbaratov05@gmail.com>
2022-08-25 18:55:17 +0300

    deleted gitignore

--- clients/admin.py
+++ clients/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Client
+from .models import Client, Order
 
 
 admin.site.register(Client)
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    list_display = ["name", "contacts", "created_at", "finished"]
+    list_editable = ["contacts", "finished"]
+    fields = ["name", "contacts", "created_at", "updated_at", "description", "finished"]
+    readonly_fields = ["created_at", "updated_at"]
+
+admin.site.register(Order, OrderAdmin)
--- clients/models.py
+++ clients/models.py
@@ -12,3 +12,27 @@ class Client(models.Model):
         null=True,
         blank=True
     )
+
+class Order(models.Model):
+    created_at = models.DateTimeField(
+        verbose_name="Дата и время заказа",
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        verbose_name="Дата и время изменения заказа",
+        auto_now=True,
+    )
+
+    description = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=255)
+    contacts = models.CharField(max_length=255)
+    finished = models.BooleanField(default=False)
+
+
+
+    def __str__(self):
+        return f"{self.name} - {self.contacts}:"
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
